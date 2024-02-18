883632c6a6c342ff97dd96650ef83b7edc79d9fd
Ezra Mays Richards <ezra1almond@gmail.com>
2024-02-17 23:20:06 -0500

    bio update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,52 @@
 import React from 'react';
+import bioContent from './bioContent';
 
 function Home() {
 
     return (
         <div className="home-container" >
 
-            <div className="home-card-wrapper" >
-
-                <div className="home-card" >
-
-                    <div className="home-card-image">
-                        <img src="images/profile-pic.jpg" alt="profile one"/>
-                    </div>
-
-                    <ul className="social-icons">
-                        <li>
-                            <a href="">
-                                <i className="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fab fa-instagram"></i>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <div className="details">
-                        <h2>Ezra Mays Richards</h2>
-                        <h3 className="job-title">Web Developer</h3> 
-                    </div>
-                </div>
+            <div className="home-header-wrapper" >
+                <h1 className="home-header">Welcome to my workspace...</h1>
             </div>
 
-            <div className="home-bio-wrapper">
-                <h4 className="home-bio">Welcome to my porfolio and project hub. I am a full-stack web developer with a drive for creativity and ingenuity. Feel free to check out my work and reach out to make friends.</h4>
+            <div className="home-content-container" >
+                <div className="home-card-wrapper" >
+
+                    <div className="home-card" >
+
+                        <div className="home-card-image">
+                            <img src="images/profile-pic.jpg" alt="profile one" />
+                        </div>
+
+                        <ul className="social-icons">
+                            <li>
+                                <a href="">
+                                    <i className="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <i className="fab fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <div className="details">
+                            <h2>Ezra Mays Richards</h2>
+                            <h3 className="job-title">Web Developer</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="home-bio-wrapper">
+                    <h4 className="home-bio">{bioContent}</h4>
+                </div>
+
+            </div>
+
+            <div className="home-tech-wrapper">
+                <h2 className="home-tech-header">Technologies:</h2>
             </div>
 
 
---
+++ src/components/bioContent.js
@@ -0,0 +1,9 @@
+const bioContent = `
+  My name is Ezra. I am a full-stack web developer with a drive for creativity and ingenuity.
+  I aim to build and contribute towards platforms that can make a positive impact and innovate the world around us.
+  I see technology as a driving force of our future, and I strive to create technical solutions that are built with purpose.
+  Feel free to check out some of my projects on the next page, or hover above my photo for my contact info!
+`;
+
+export default bioContent;
+
